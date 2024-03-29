2578613d7fb7d3a59e7b6a7b2711d64d0051ec24
Noah Schoeters <noah.schoeters@student.uhasselt.be>
2024-03-29 11:44:16 +0100

    First commit/ initalization of project

---
+++ CGOGLP/CGOGLP/Main.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+//Functions
+
+//Settings
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+
+
+int main()
+{
+
+}
